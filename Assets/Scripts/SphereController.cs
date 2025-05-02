4b1c6c25406d76c317557bb970b992988bf7aef8
s_ueno <pandapark09@yahoo.co.jp>
2025-05-02 12:00:59 +0900

    実験

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class SphereController : MonoBehaviour
 {
+    bool isMoveRight;
+    bool isMoveLeft;
+    bool isShooting;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -15,9 +19,13 @@ public class SphereController : MonoBehaviour
     {
        if(Input.GetKey(KeyCode.LeftArrow)){
         transform.Translate(new Vector3(1,0,0));
+        animator.SetTrigger("moveRight");
        }
         if(Input.GetKey(KeyCode.RightArrow)){
         transform.Translate(new Vector3(-1,0,0));
+        isMoveRight = true;
        } 
+       isMoveLeft=false;
+       isMoveRight = false ;
     }
 }
