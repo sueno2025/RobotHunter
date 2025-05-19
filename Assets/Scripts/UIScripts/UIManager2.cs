344081129cf5c8c400fc40b38798212e0a163d3d
s_ueno <pandapark09@yahoo.co.jp>
2025-05-19 11:00:57 +0900

    UI変更

--- Assets/Scripts/UIScripts/UIManager2.cs
+++ Assets/Scripts/UIScripts/UIManager2.cs
@@ -3,26 +3,20 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using DG.Tweening;
-
 public class UIManager2 : MonoBehaviour
 {
     public GameObject gameOverPanel;
     public GameObject gameClearPanel;
+    public GameObject pauseButton;
     public GameObject titleButton;
     public CanvasGroup gameOverCanvasGroup;
     public CanvasGroup gameClearCanvasGroup;
     public CanvasGroup titleButtonCanvasGroup;
-
-
-
     void Start()
     {
-
-
         gameOverPanel.SetActive(false);
         gameClearPanel.SetActive(false);
         titleButton.SetActive(false);
-
         gameOverCanvasGroup = gameOverPanel.GetComponentInParent<CanvasGroup>();
         gameClearCanvasGroup = gameClearPanel.GetComponentInParent<CanvasGroup>();
         titleButtonCanvasGroup = titleButton.GetComponentInParent<CanvasGroup>();
@@ -30,25 +24,25 @@ public class UIManager2 : MonoBehaviour
         gameClearCanvasGroup.alpha = 0f;
         titleButtonCanvasGroup.alpha = 0f;
     }
-
     public void ShowGameOver()
     {
+        if (pauseButton != null)
+            pauseButton.SetActive(false);
         gameOverPanel.SetActive(true);
         gameClearPanel.SetActive(false);
         titleButton.SetActive(true);
         gameOverCanvasGroup.DOFade(1f, 3f);
-        titleButtonCanvasGroup.DOFade(1f, 3f);
-
-
+        titleButtonCanvasGroup.DOFade(1f, 3f).SetDelay(1F);
     }
-
     public void ShowGameClear()
     {
+        if (pauseButton != null)
+            pauseButton.SetActive(false);
         gameOverPanel.SetActive(false);
         gameClearPanel.SetActive(true);
         titleButton.SetActive(true);
         gameClearCanvasGroup.DOFade(1f, 3f);
-        titleButtonCanvasGroup.DOFade(1f, 3f);
+        titleButtonCanvasGroup.DOFade(1f, 3f).SetDelay(1F);
     }
     public void ReturnToMainMenu()
     {
